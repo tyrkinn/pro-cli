76c6a21faf80d50ec406c6f6a076d2b61811f9fd
tyrkinn <nikemad61@gmail.com>
2022-05-27 20:31:01 +0300

    Feature: project language detection

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,30 @@
-use std::{fs::{self, DirEntry}, process::Command};
 use clap::Parser;
+use std::{
+    fs::{self, DirEntry},
+    process::Command,
+};
 
 const PROJECT_DIR_URL: &str = "/Users/tyrkinn/github/tyrkinn";
+const TYPESCRIPT_INDICATOR_FILE: &str = "tsconfig.json";
+const RUST_INDICATOR_FILE: &str = "Cargo.toml";
 
-/// Cli to list and open projects
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    /// Directory where projects are stored
     #[clap(short, long, default_value = PROJECT_DIR_URL)]
     project_dir: String,
 
-    /// List projects
     #[clap(short, long)]
     list_projects: bool,
 
-    /// Provide project name to open in vscode
     #[clap(short, long)]
     open_project: Option<String>,
 }
 
-
+enum ProjectType {
+    Typescript,
+    Rust
+}
 
 fn is_hidden(entry: &DirEntry) -> bool {
     entry
@@ -30,7 +34,7 @@ fn is_hidden(entry: &DirEntry) -> bool {
         .unwrap_or(false)
 }
 
-fn get_projects(dir_url: String) -> Vec<std::string::String> {
+fn get_projects(dir_url: &String) -> Vec<std::string::String> {
     let dirs = fs::read_dir(dir_url).unwrap();
     dirs.filter_map(|f| f.ok())
         .filter(|f| f.file_type().unwrap().is_dir() && !is_hidden(f))
@@ -38,31 +42,56 @@ fn get_projects(dir_url: String) -> Vec<std::string::String> {
         .collect::<Vec<String>>()
 }
 
-fn list_dir(dir_url: String) {
-        get_projects(dir_url)
+fn list_dir(dir_url: &String) {
+    get_projects(dir_url)
         .into_iter()
-        .for_each(|f| println!("{}", f))
+        .for_each(|f| {
+            let project_type = get_project_language(&f);
+            if project_type.is_some() {
+                let project_lang = match project_type.unwrap() {
+                    ProjectType::Typescript => "Typescript",
+                    ProjectType::Rust => "Rust"
+                };
+                println!("{} - {}", f, project_lang);
+            }
+            else {
+                println!("{}", f);
+            }
+        })
 }
 
-fn open_project(project_name: String, dir_url: String) {
-    if get_projects(dir_url.clone()).contains(&project_name) {
+fn open_project(project_name: &String, dir_url: &String) {
+    if get_projects(dir_url).contains(project_name) {
         Command::new("code")
-        .arg("-r")
-        .arg(format!("{}/{}", dir_url, project_name))
-        .output()
-        .expect("Error");
-    } 
-    else {
+            .arg("-r")
+            .arg(format!("{}/{}", dir_url, project_name))
+            .output()
+            .expect("Error");
+    } else {
         println!("Project with provided name does not exists");
+    }
+}
+
+fn get_project_language(project_name: &String) -> Option<ProjectType> {
+    let project_full_path: String = format!("{}/{}", PROJECT_DIR_URL, project_name);
+    let project_files = fs::read_dir(project_full_path)
+        .unwrap()
+        .map(|x| x.unwrap().file_name().into_string().unwrap())
+        .collect::<Vec<String>>();
+    if project_files.contains(&TYPESCRIPT_INDICATOR_FILE.to_owned()) {
+        return Some(ProjectType::Typescript);
+    } else if project_files.contains(&RUST_INDICATOR_FILE.to_owned()) {
+        return Some(ProjectType::Rust);
+    } else {
+        return None;
     }
 }
 
 fn main() {
     let args = Args::parse();
     if args.list_projects {
-        list_dir(args.project_dir.clone())
-    }
-    else if args.open_project.is_some() {
-        open_project(args.open_project.unwrap(), args.project_dir);
+        list_dir(&args.project_dir)
+    } else if args.open_project.is_some() {
+        open_project(&args.open_project.unwrap(), &args.project_dir);
     }
 }
