68cdd9c5c1e4120687fda35532cce6be9903abfa
Nikita Tyrkin <tyrkinn@pm.me>
2022-10-28 20:00:50 +0300

    :goal_net: Add readable error msgs to config.rs

--- src/config.rs
+++ src/config.rs
@@ -2,6 +2,7 @@ use home;
 use serde_derive::{Deserialize, Serialize};
 use std::fs::read_to_string;
 use std::io::Write;
+use std::process::exit;
 use std::{
     fs::{self, File},
     path::Path,
@@ -16,7 +17,10 @@ pub struct ProConfig {
 }
 
 fn home_dir() -> String {
-    let home_dir_path = home::home_dir().expect("Can't get home_dir");
+    let home_dir_path = home::home_dir().unwrap_or_else(|| {
+        eprintln!("Can't get your home directory because of:\n");
+        exit(1);
+    });
     home_dir_path.to_str().unwrap().to_owned()
 }
 
@@ -39,8 +43,14 @@ pub fn create_config_file() {
     let config_path_string = &config_path();
     let config_path = Path::new(config_path_string);
     let prefix = config_path.parent().unwrap();
-    fs::create_dir_all(prefix).expect("Can't create prefix folder");
-    File::create(config_path).expect("Can't create config file");
+    fs::create_dir_all(prefix).unwrap_or_else(|e| {
+        eprintln!("Can't create prefix folder because of:\n{}", e);
+        exit(1);
+    });
+    File::create(config_path).unwrap_or_else(|e| {
+        eprintln!("Can't create config because of:\n{}", e);
+        exit(1);
+    });
 }
 
 pub fn write_config(config: &ProConfig) {
@@ -48,13 +58,21 @@ pub fn write_config(config: &ProConfig) {
         .write(true)
         .open(config_path())
         .expect("Can't open config file");
-    let default_toml_config = toml::to_string(config).expect("Can't serialize `ProConfig` to toml");
+    let default_toml_config = toml::to_string(config).unwrap_or_else(|e| {
+        eprintln!("Can't serialize `ProConfig` to toml because of:\n {}", e);
+        exit(1);
+    });
     write!(config_file, "{}", default_toml_config).expect("Can't write default config");
 }
 
 pub fn read_config() -> ProConfig {
-    let file_contents = read_to_string(config_path()).expect("Can't read config file");
-    let config: ProConfig =
-        toml::from_str(file_contents.as_ref()).expect("Can't deserialize config file contents");
+    let file_contents = read_to_string(config_path()).unwrap_or_else(|e| {
+        eprintln!("Can't read config contents because of:\n{}", e);
+        exit(1);
+    });
+    let config: ProConfig = toml::from_str(file_contents.as_ref()).unwrap_or_else(|e| {
+        eprintln!("Can't deserialize config file content because of:\n{}", e);
+        exit(1);
+    });
     config
 }
